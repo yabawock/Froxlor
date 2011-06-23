8fa01332cf190e3cae86b414ef60ef301ac8a4ed
Andreas Burchert (scarya) <scarya@froxlor.org>
2011-06-23 12:57:16 +0200

    Added constructor ro rrp class, refs #681

--- lib/classes/dms/class.rrp.php
+++ lib/classes/dms/class.rrp.php
@@ -12,25 +12,44 @@
  * @author     Froxlor team <team@froxlor.org> (2010-)
  * @author     Andreas Burchert <scarya@froxlor.org>
  * @license    GPLv2 http://files.froxlor.org/misc/COPYING.txt
- * @package    Cron
- * @version    $Id$
+ * @package    DMS
  */
 
 class rrp implements dms
 {
-	public function handleCreate() {
+	private $_user;
+	private $_password;
+	private $_opmode;
+	private $_socket;
+	
+	private $_config = array();
+	
+	public function __construct($user, $password, $opmode = "ote", $socket = "http://api-ote.rrpproxy.net/call?") {
+		$this->config = array("username" => $user,
+						"password" => $password,
+						"opmode" => $opmode,
+						"socket" => $socket);
+		
+		$this->_user = $user;
+		$this->_password = $password;
+		$this->_opmode = $opmode;
+		$this->_socket = $socket;
+	}
+	
+	public function handleCreate($handle) {
 		
 	}
 	
-	public function handleDelete() {
+	public function handleDelete($handle) {
 		
 	}
 	
-	public function handleAlter() {
+	public function handleAlter($handle) {
 		
 	}
 	
 	public function handleList() {
-		
+		$request = new MREG_RequestHttp($this->config);
+		$response = $request->send(array("command" => "QueryContactList"));
 	}
 }
