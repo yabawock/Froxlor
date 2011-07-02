e6203f498a9fe0d4fd7309ec8d33d6c168c84411
Florian Aders (EleRas) <eleras@froxlor.org>
2011-07-02 11:39:24 +0200

    More formats for field - validation, refs #821 @0h15m
    Signed-off-by: Florian Aders (EleRas) <eleras@froxlor.org>

--- lib/classes/validate/class.validateForm.php
+++ lib/classes/validate/class.validateForm.php
@@ -46,12 +46,17 @@ class validateForm
 				// Maybe it is an unlimited field?
 				elseif(substr($formname, -3) == "_ul" && array_key_exists(substr($formname, 0, strlen($formname) - 3), $referenceForm['sections'][$sectionname]['fields']))
 				{
-					if ($data[$formname] != "1" && $data[$formname] != "0")
+					var_dump($data[$formname]);
+					if ($data[$formname] != "-1")
 					{
 						$failed[$formname] = array(
-							'label' => $referenceForm['sections'][$sectionname]['fields'][substr($formname, 0, strlen($formname) - 3)]['label'] . "(" . _('Unlimited') . ")",
-							'wrongvalue' => _('Unlimited needs to be boolean (0|1)')
+							'label' => $referenceForm['sections'][$sectionname]['fields'][substr($formname, 0, strlen($formname) - 3)]['label'] . " (" . _('Unlimited') . ")",
+							'wrongvalue' => _('Unlimited need to be -1 if given')
 						);
+					}
+					else
+					{
+						$safe[$formname] = "-1";
 					}
 					unset($data[$formname]);
 					break;
@@ -73,6 +78,11 @@ class validateForm
 			{
 				return true;
 			}
+			if ($rules['required'] == 1 && strlen($value) == 0)
+			{
+				$result[] = _('The field is required but no value was given.');
+				$safe = 0;
+			}
 
 			// Loop to all given rules
 			foreach($rules as $key => $rule)
@@ -82,45 +92,125 @@ class validateForm
 					case 'minlen':
 						if (strlen($value) < $rule)
 						{
-							$result[] = sprintf(_('The minimum required length for this field is %d'), $rule);
+							$result[] = sprintf(_('The minimum required length for this field is %d.'), $rule);
 							$safe = 0;
 						}
 						break;
 					case 'maxlen':
 						if (strlen($value) > $rule)
 						{
-							$result[] = sprintf(_('The maximum allowed length for this field is %d'), $rule);
+							$result[] = sprintf(_('The maximum allowed length for this field is %d.'), $rule);
 							$safe = 0;
 						}
 						break;
 					case 'minvalue':
 						if (!is_numeric($value) || $value > $rule)
 						{
-							$result[] = sprintf(_('The value has to be greater than %d'), $rule);
+							$result[] = sprintf(_('The value has to be greater than %d.'), $rule);
 							$safe = 0;
 						}
 						break;
 					case 'maxvalue':
 						if (!is_numeric($value) || $value < $rule)
 						{
-							$result[] = sprintf(_('The value has to be smaller than %d'), $rule);
+							$result[] = sprintf(_('The value has to be smaller than %d.'), $rule);
 							$safe = 0;
 						}
 						break;
 					case 'format':
 						if ($rule == 'email')
 						{
-							if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+							if (!filter_var($value, FILTER_VALIDATE_EMAIL))
 							{
-								$result[] = _('The value has to be an e-mail');
+								$result[] = _('The value has to be an e-mail.');
 								$safe = 0;
+							}
+						}
+						elseif($rule == 'phone')
+						{
+							if (!preg_match('/^[0-9\- \+\(\)\/]*$/', $value))
+							{
+								$result[] = _('The value contains invalid characters.');
+								$safe = 0;
+							}
+						}
+						elseif($rule == 'zipcode')
+						{
+							if (!preg_match('/^[0-9 \-A-Z]$/', $value))
+							{
+								$result[] = _('The value contains invalid characters for a zipcode. Only numbers, letters, spaces and dashes are allowed.');
+								$safe = 0;
+							}
+						}
+						elseif($rule == 'customernumber')
+						{
+							if (!preg_match('/^[A-Za-z0-9 \-]*$/Di', $value))
+							{
+								$result[] = _('The value contains invalid characters for a customer number. Only numbers, letters, spaces and dashes are allowed.');
+								$safe = 0;
+							}
+						}
+						elseif($rule == 'number')
+						{
+							if (!preg_match('/^\d+$/', $value))
+							{
+								$result[] = _('The value is not a valid number');
+								$safe = 0;
+							}
+						}
+						elseif($rule == 'decimal')
+						{
+							if (!preg_match('/^\d+(?:[,\.]\d+)?$/', $value))
+							{
+								$result[] = _('The value is not a valid decimal number');
+								$safe = 0;
+							}
+						}
+						elseif($rule == 'boolean')
+						{
+							if (!preg_match('/^\d+$/', $value))
+							{
+								$result[] = _('The value is not a valid boolean. Only 0 or 1 allowed');
+								$safe = 0;
+							}
+						}
+						elseif($rule == 'string')
+						{
+							if (preg_match('/[\r\n\t\f\0]/', $value))
+							{
+								$result[] = _('The value contains invalid characters.');
+								$safe = 0;
+							}
+						}
+						elseif($rule == 'password')
+						{
+							if (preg_match('/[\r\n\t\f\0]/', $value))
+							{
+								$result[] = _('The value contains invalid characters.');
+								$safe = 0;
+							}
+							if (getSetting('panel', 'password_min_length') > 0)
+							{
+								if (strlen($value) < getSetting('panel', 'password_min_length'))
+								{
+									$result[] = sprintf(_('The minimum required length for this field is %d.'), getSetting('panel', 'password_min_length'));
+									$safe = 0;
+								}
+							}
+							if (getSetting('panel', 'password_regex') != '')
+							{
+								if (!preg_match(getSetting('panel', 'password_regex'), $value))
+								{
+									$result[] = _('The password is not complex enough.');
+									$safe = 0;
+								}
 							}
 						}
 						break;
 					case 'regex':
 						if (!preg_match('/^' . $rule . '$/', $value))
 						{
-							$result[] = sprintf(_('The value does not match the required format: %s'), $rule);
+							$result[] = sprintf(_('The value does not match the required format: %s.'), $rule);
 							$safe = 0;
 						}
 						break;
