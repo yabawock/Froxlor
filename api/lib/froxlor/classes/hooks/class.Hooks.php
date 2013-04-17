1c9a1f1f63b2c284c517faaa0fe8e323e1a938db
Michael Kaufmann (d00p) <d00p@froxlor.org>
2013-04-17 22:23:48 +0200

    add caching for hook-calls so we do not need to iterate through all module all the time, thx Eleras
    Signed-off-by: Michael Kaufmann (d00p) <d00p@froxlor.org>

--- api/lib/froxlor/classes/hooks/class.Hooks.php
+++ api/lib/froxlor/classes/hooks/class.Hooks.php
@@ -22,6 +22,8 @@
  * @since      0.99.0
  */
 
+define('HOOK_CACHE_TIME', 60 * 60 * 3); // 3 hours
+
 /**
  * Class Hooks
  *
@@ -33,7 +35,7 @@
  * @category   core
  * @package    API
  * @since      0.99.0
- */
+*/
 class Hooks {
 
 	/**
@@ -48,61 +50,94 @@ class Hooks {
 	 * @return nothing
 	 */
 	public static function callHooks($hookname = null, $params = null) {
-		// modules path
-		$paths = array(
-				FROXLOR_API_DIR . '/modules/',
-		);
 
-		// now iterate through the paths
-		foreach ($paths as $path) {
-			// valid directory?
-			if (is_dir($path)) {
-				// create RecursiveIteratorIterator
-				$its = new RecursiveIteratorIterator(
-						new RecursiveDirectoryIterator($path)
-				);
+		$_cachefile = dirname(__FILE__).'/~hookscache.'.$hookname.'.php';
 
-				// check every file
-				foreach ($its as $fullFileName => $it ) {
-					// does it match the Filename pattern?
-					if (preg_match("/^module\.(.+)\.php$/i", $it->getFilename())) {
-						// indicator if we can call the function in that module
-						$useable = true;
-						// get module-name out of filename
-						$module = substr($it->getFilename(), 7, -4);
-						// check whether function exists in module
-						try {
-							$refl = new ReflectionMethod($module, $hookname);
-							if ($refl->isPublic() == false) {
-								// hook-function found but is not available for us :/
+		// check for cache
+		$_recache = true;
+		if (@file_exists($_cachefile)) {
+			$_recache = false;
+			require $_cachefile;
+			if (($cache['timestamp'] + HOOK_CACHE_TIME) > time()) {
+				@unlink($cachefile);
+				$_recache = true;
+			}
+		}
+
+		if ($_recache) {
+
+			$cachedata = array();
+
+			// modules path
+			$paths = array(
+					FROXLOR_API_DIR . '/modules/',
+			);
+			// now iterate through the paths
+			foreach ($paths as $path) {
+				// valid directory?
+				if (is_dir($path)) {
+					// create RecursiveIteratorIterator
+					$its = new RecursiveIteratorIterator(
+							new RecursiveDirectoryIterator($path)
+					);
+
+					// check every file
+					foreach ($its as $fullFileName => $it ) {
+						// does it match the Filename pattern?
+						if (preg_match("/^module\.(.+)\.php$/i", $it->getFilename())) {
+							// indicator if we can call the function in that module
+							$useable = true;
+							// get module-name out of filename
+							$module = substr($it->getFilename(), 7, -4);
+							// check whether function exists in module
+							try {
+								$refl = new ReflectionMethod($module, $hookname);
+								if ($refl->isPublic() == false) {
+									// hook-function found but is not available for us :/
+									$useable = false;
+								}
+							} catch (Exception $e) {
+								// not found, never mind
 								$useable = false;
 							}
-						} catch (Exception $e) {
-							// not found, never mind
-							$useable = false;
-						}
-						// call the hook-function
-						if ($useable) {
-							try {
-								$mod = new $module();
-								$mod->{$hookname}($params);
-							} catch (Exception $e) {
-								// log error but go on
-								// FIXME logging
+							// call the hook-function
+							if ($useable) {
+								$cachedata[] = $module;
 							}
+							/**
+							 * call_user_func does not pass the $params variable
+							 * as reference so a hook-function cannot change its
+							 * data :/
+							 *
+							 call_user_func(array($mod, $hookname), $params);
+							 */
 						}
-						/**
-						 * call_user_func does not pass the $params variable
-						 * as reference so a hook-function cannot change its
-						 * data :/
-						 *
-						 call_user_func(array($mod, $hookname), $params);
-						*/
 					}
+
+					$fh = fopen($_cachefile, 'w');
+					if ($fh) {
+						$cfdata = "<?php\n\$cachedata = array();\n";
+						foreach ($cachedata as $module) {
+							$cfdata .= "\$cachedata[] = '".$module."';\n";
+						}
+						fwrite($fh, $cfdata);
+						fclose($fh);
+					}
+				} else {
+					// yikes - no valid directory to check
+					break;
 				}
-			} else {
-				// yikes - no valid directory to check
-				break;
+			}
+		}
+
+		foreach ($cachedata as $module) {
+			try {
+				$mod = new $module();
+				$mod->{$hookname}($params);
+			} catch (Exception $e) {
+				// log error but go on
+				ApiLogger::error("Error in execution of hook '".$hookname."'");
+				ApiLogger::error($e->getMessage());
 			}
 		}
 	}
