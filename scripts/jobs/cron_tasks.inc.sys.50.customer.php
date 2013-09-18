59fc3b883ccff56d7391847d1238f35e8e0cfc68
Morton Jonuschat <m.jonuschat@gute-botschafter.de>
2013-09-18 07:50:37 +0200

    Split customer class into smaller functions

--- scripts/jobs/cron_tasks.inc.sys.50.customer.php
+++ scripts/jobs/cron_tasks.inc.sys.50.customer.php
@@ -36,16 +36,28 @@ class customer
   public $settings = array();
   public $customerData = array();
 
+  /**
+   * Constructor for the customer class
+   * @param db $db                      Database connection
+   * @param FroxlorLogger $logger       Logging instance
+   * @param resource $debugHandler      Debug handler
+   * @param array $settings             System settings
+   * @param array $customerData         Custome details
+   */
   public function __construct($db, $logger, $debugHandler, $settings, $customerData) {
-
     $this->db = $db;
     $this->logger = $logger;
     $this->debugHandler = $debugHandler;
     $this->settings = $settings;
     $this->customerData = $customerData;
-
+    $this->userHomeDir = '';
+    $this->userMailDir = '';
   }
 
+  /**
+   * Creates the home and utility directories for a new customer
+   * @return void
+   */
   public function createHomeDir()
   {
     fwrite($debugHandler, '  cron_tasks: Task2 started - create new home' . "\n");
@@ -53,38 +65,79 @@ class customer
 
     if(is_array($this->customerData) && !empty($this->customerData))
     {
-      // define paths
-      $userhomedir = makeCorrectDir($this->settings['system']['documentroot_prefix'] . '/' . $this->customerData['loginname'] . '/');
-      $usermaildir = makeCorrectDir($this->settings['system']['vmail_homedir'] . '/' . $this->customerData['loginname'] . '/');
-
-      // stats directory
-      if($this->settings['system']['awstats_enabled'] == '1')
-      {
-        $cronlog->logAction(CRON_ACTION, LOG_NOTICE, 'Running: mkdir -p ' . escapeshellarg($userhomedir . 'awstats'));
-        safe_exec('mkdir -p ' . escapeshellarg($userhomedir . 'awstats'));
-      } else {
-        $cronlog->logAction(CRON_ACTION, LOG_NOTICE, 'Running: mkdir -p ' . escapeshellarg($userhomedir . 'webalizer'));
-        safe_exec('mkdir -p ' . escapeshellarg($userhomedir . 'webalizer'));
-      }
-
-      // maildir
-      $cronlog->logAction(CRON_ACTION, LOG_NOTICE, 'Running: mkdir -p ' . escapeshellarg($usermaildir));
-      safe_exec('mkdir -p ' . escapeshellarg($usermaildir));
-
-      //check if admin of customer has added template for new customer directories
-      if((int)$this->customerData['store_defaultindex'] == 1)
-      {
-        storeDefaultIndex($this->customerData['loginname'], $userhomedir, $cronlog, true);
-      }
-
-      // strip of last slash of paths to have correct chown results
-      $userhomedir = (substr($userhomedir, 0, -1) == '/') ? substr($userhomedir, 0, -1) : $userhomedir;
-      $usermaildir = (substr($usermaildir, 0, -1) == '/') ? substr($usermaildir, 0, -1) : $usermaildir;
-
-      $cronlog->logAction(CRON_ACTION, LOG_NOTICE, 'Running: chown -R ' . (int)$this->customerData['uid'] . ':' . (int)$this->customerData['gid'] . ' ' . escapeshellarg($userhomedir));
-      safe_exec('chown -R ' . (int)$this->customerData['uid'] . ':' . (int)$this->customerData['gid'] . ' ' . escapeshellarg($userhomedir));
-      $cronlog->logAction(CRON_ACTION, LOG_NOTICE, 'Running: chown -R ' . (int)$this->settings['system']['vmail_uid'] . ':' . (int)$this->settings['system']['vmail_gid'] . ' ' . escapeshellarg($usermaildir));
-      safe_exec('chown -R ' . (int)$this->settings['system']['vmail_uid'] . ':' . (int)$this->settings['system']['vmail_gid'] . ' ' . escapeshellarg($usermaildir));
+      $this->createStatsDir();
+      $this->createMailDir();
+      $this->copyDefaultIndex();
+      $this->chownHomeDir();
     }
+  }
+
+  /**
+   * Initialize the paths needed for later steps
+   * @return void
+   */
+  protected function definePaths()
+  {
+  // define paths
+    $this->userHomeDir = makeCorrectDir($this->settings['system']['documentroot_prefix'] . '/' . $this->customerData['loginname'] . '/');
+    $this->userMailDir = makeCorrectDir($this->settings['system']['vmail_homedir'] . '/' . $this->customerData['loginname'] . '/');
+  }
+
+  /**
+   * Create the directory for the web statistics
+   * @return void
+   */
+  protected function createStatsDir()
+  {
+    // stats directory
+    if($this->settings['system']['awstats_enabled'] == '1')
+    {
+      $cronlog->logAction(CRON_ACTION, LOG_NOTICE, 'Running: mkdir -p ' . escapeshellarg($this->userHomeDir . 'awstats'));
+      safe_exec('mkdir -p ' . escapeshellarg($this->userHomeDir . 'awstats'));
+    } else {
+      $cronlog->logAction(CRON_ACTION, LOG_NOTICE, 'Running: mkdir -p ' . escapeshellarg($this->userHomeDir . 'webalizer'));
+      safe_exec('mkdir -p ' . escapeshellarg($this->userHomeDir . 'webalizer'));
+    }
+  }
+
+  /**
+   * Create the mail storage dir
+   * @return void
+   */
+  protected function createMailDir()
+  {
+    // maildir
+    $cronlog->logAction(CRON_ACTION, LOG_NOTICE, 'Running: mkdir -p ' . escapeshellarg($this->userMailDir));
+
+    safe_exec('mkdir -p ' . escapeshellarg($this->userMailDir));
+  }
+
+  /**
+   * Copy the default index page to the customer directory
+   * @return void
+   */
+  protected function copyDefaultIndex()
+  {
+    //check if admin of customer has added template for new customer directories
+    if((int)$this->customerData['store_defaultindex'] == 1)
+    {
+      storeDefaultIndex($this->customerData['loginname'], $this->userHomeDir, $cronlog, true);
+    }
+  }
+
+  /**
+   * Change the owner of the customer homedir
+   * @return void
+   */
+  protected function chownHomeDir()
+  {
+    // strip of last slash of paths to have correct chown results
+    $this->userHomeDir = (substr($this->userHomeDir, 0, -1) == '/') ? substr($this->userHomeDir, 0, -1) : $this->userHomeDir;
+    $this->userMailDir = (substr($this->userMailDir, 0, -1) == '/') ? substr($this->userMailDir, 0, -1) : $this->userMailDir;
+
+    $cronlog->logAction(CRON_ACTION, LOG_NOTICE, 'Running: chown -R ' . (int)$this->customerData['uid'] . ':' . (int)$this->customerData['gid'] . ' ' . escapeshellarg($this->userHomeDir));
+    safe_exec('chown -R ' . (int)$this->customerData['uid'] . ':' . (int)$this->customerData['gid'] . ' ' . escapeshellarg($this->userHomeDir));
+    $cronlog->logAction(CRON_ACTION, LOG_NOTICE, 'Running: chown -R ' . (int)$this->settings['system']['vmail_uid'] . ':' . (int)$this->settings['system']['vmail_gid'] . ' ' . escapeshellarg($this->userMailDir));
+    safe_exec('chown -R ' . (int)$this->settings['system']['vmail_uid'] . ':' . (int)$this->settings['system']['vmail_gid'] . ' ' . escapeshellarg($this->userMailDir));
   }
 }
